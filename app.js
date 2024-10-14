ea8d306405d4b452fd0ced4f103f6a641b9d8063
yushafahad17 <fahadxml5@gmail.com>
2024-10-15 03:47:18 +0700

    yaudah mas

--- app.js
+++ app.js
@@ -159,3 +159,7 @@ app.use((req, res, next) => {
     console.log(`Request received: ${req.method} ${req.url}`);
     next();
 });
+
+app.get('/test', (req, res) => {
+  res.send('Test endpoint working!');
+});
